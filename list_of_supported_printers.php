c2a1a53acf31f9ad86914bf108513cb5d968fad1
࿇ Ambjoern E.S.A <archangel.ambriel@icloud.com>
2020-06-18 23:28:24 +0200

    list_of_supported_printers.php

---
+++ list_of_supported_printers.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta property="og:url" content="http://printhand.com/download.php?locale=en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="PrintHand Mobile Print">
+    <meta property="og:description" content="I'm printing directly from my mobile device and you can do it too! Application available on Android, iOS, Windows Phone, Blackberry">
+    <meta property="og:image" content="http://printhand.com/images/icon512x512.png">
+    <meta property="fb:app_id" content="440688562642936">
+    <title>PrintHand. List of Supported Printers.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="follow"></meta>
+    <meta name="revisit-after" content="5 days"></meta>
+    <link rel="canonical" href="http://printhand.com/list_of_supported_printers.php" />
+    <link type="text/css" href="/fonts.css" rel="stylesheet" />
+    <link type="text/css" href="/styles2.css" rel="stylesheet" />
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <script src="/js/search.js"></script>
+  </head>
+  <body>
+
+<header>
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <div class="margin6small">
+            <a class="toolbar-btn-a btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <img src="/images/menu.png">
+            </a>
+            <div class="nav-collapse collapse">
+              <ul class="nav pull-left">
+                <li><a href="index.php" class="toolbar-btn">Home</a></li><li><a href="download.php" class="toolbar-btn">Install</a></li><li><a href="screen_shots.php" class="toolbar-btn">Screen Shots</a></li><li><a href="faq.php" class="toolbar-btn">FAQ</a></li><li><a href="help.php" class="toolbar-btn-a">Help</a></li><li><a href="ph_troubleshooting.php" class="toolbar-btn">Troubleshooting</a></li><li><a href="about_us.php" class="toolbar-btn">About Us</a></li><li><a href="printhand_for_business.php" class="toolbar-btn">For Business</a></li><li><a href="integration.php" class="toolbar-btn">For Developers</a></li>                
+              </ul>
+              <ul class="nav pull-right">
+                <li><a href="feedback.php" class="toolbar-btn">Feedback</a></li>              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<div class="dark">
+<div class="container">
+  <div class="margin6small">
+  <table class="headertable">
+    <tr>
+      <td class="logo-holder">
+
+<a href="index.php" class="logo"><img src="/images/logo-x2.png" width="174" height="76"></a>
+<a href="install_android.php" title="Available on Android platform" class="top-icon vizible_big"><img src="/images/icon-android.png"></a>
+<a href="install_ios.php" title="Available on iOS platform" class="top-icon vizible_big"><img src="/images/icon-ios.png"></a>
+  <a href="install_windows_phone.php" title="Available on Windows Phone platform" class="top-icon vizible_big"><img src="/images/icon-winphone.png"></a>
+  <a href="install_blackberry.php" title="Available on Blackberry platform" class="top-icon vizible_big"><img src="/images/icon-blackberry.png"></a>
+  <a href="install_chrome.php" title="Available for Chrome" class="top-icon vizible_big_only"><img src="/images/icon-chrome-os.png"></a>
+
+      </td>
+      <td align="right" style="padding-top:10px">
+
+<table>
+   <tr>
+      <td class="search-panel" colspan="2">
+  <a href="https://twitter.com/dynamixsoftware" title="Follow us on Twitter" class="btn vizible_big_only"><img src="/images/twitter.png"></a>
+  <a href="https://www.facebook.com/pages/Mobile-Dynamix/120680297971086" title="Follow us on Facebook" class="btn vizible_big_only"><img src="/images/facebook.png"></a>
+
+  <form action="https://google.com/search" name="theForm" onsubmit="return search();" class="search-form no-small">
+  <input type="hidden" name="q"></input>
+  <form>
+    <input type="text" placeholder='Search' id="x"><a href="javascript:if(search()) document.theForm.submit()" class="btn-go"><img src="/images/arrow-x2.png" width="20" height="20"></a>
+  </form>
+
+<div class="btn-group">
+  <button class="btn btn-small dropdown-toggle" data-toggle="dropdown">English <img src="/images/carpet-x2.png" width="10" height="6"></button>
+  <ul class="dropdown-menu pull-right">
+    <li><a href="/en/list_of_supported_printers.php">English</a></li>
+          <li><a href="/ru/list_of_supported_printers.php">Русский</a></li>
+        <li><a href="/cn/list_of_supported_printers.php">中文（简体)</a></li>
+  </ul>
+</div>
+
+    </td>
+  </tr>
+  <tr>    
+    <td class="seb"><div style="background: transparent url('/images/se_bl.png') no-repeat left top;height:20px;"></div></td>
+    <td class="seb"><div style="background: transparent url('/images/se_br.png') no-repeat right top;height:20px;"></div></td>
+  <tr>
+</table>
+
+      </td>
+    <tr>
+  </table>
+
+  </div>
+</div>
+</div>
+</header>
+
+<div class="container">
+  <div class="margin6small">
+    <div class="row-fluid">
+      <div class="row-fluid span8">
+
+<h1>iOS, List of Supported Printers</h1><p>
+PrintHand can print to any printer as long as it's working. This is the list of compatible printers for direct 
+<a href="wifi_printers.php">Wi-Fi</a>, <a href="bluetooth_printers.php">Bluetooth</a> or 
+<a href="usb_printers.php">USB</a> printing without a computer. If you don't find your printer there, it may still work just fine. Please try printing the test page. In any case, you can always opt for printing via computer which is guaranteed to work with any printer as long as you can print to it from the computer itself. You'll need to install our free software for <a href="download_mac_client.php">Mac</a> or <a href="download_windows_client.php">PC</a>.
+</p>
+<ul class='simple'><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=able'>Able</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=apollo'>Apollo</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=apple'>Apple</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=brother'>Brother</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=canon'>Canon</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=ciaat'>CIAAT</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=citizen'>Citizen</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=compaq'>Compaq</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=dai'>Dai</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=datamax-oneil'>Datamax-ONeil</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=dec'>DEC</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=dnp'>DNP</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=epson'>Epson</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=fujifilm'>Fujifilm</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=fujitsu'>Fujitsu</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=generic'>Generic</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=gestetner'>Gestetner</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=hp'>HP</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=ibm'>IBM</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=infotec'>Infotec</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=kodak'>Kodak</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=kodak-dock-plus
+'>Kodak-Dock-Plus
+</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=kodak-dock-plus-s3
+'>Kodak-Dock-Plus-S3
+</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=kyocera'>Kyocera</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=lanier'>Lanier</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=lexmark'>Lexmark</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=minolta'>Minolta</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=mitsubishi'>Mitsubishi</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=nec'>NEC</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=nrg'>NRG</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=oki'>Oki</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=olivetti'>Olivetti</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=olympus'>Olympus</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=panasonic'>Panasonic</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=pcpi'>PCPI</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=pixma'>PIXMA</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=pixus'>PIXUS</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=raven'>Raven</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=ricoh'>Ricoh</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=samsung'>Samsung</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=savin'>Savin</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=seiko'>Seiko</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=sharp'>Sharp</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=shinko'>Shinko</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=sinfonia'>Sinfonia</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=sony'>Sony</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=star'>Star</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=tally'>Tally</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=tektronix'>Tektronix</a></li><li><a href='list_of_supported_printers.php?platform=ios&amp;vendor=xerox'>Xerox</a></li></ul> 
+      </div>
+      <div class="row-fluid span1">   
+      </div>  
+      <div class="row-fluid span3">
+
+      <h3>Help</h3>
+<ul class="right-panel">
+<li><a href="help.php">PrintHand Overview</a></li><li><a href="how_it_works.php">How It Works?</a></li><li><a href="free.php">PrintHand Free</a></li><li><a href="premium.php">PrintHand Premium</a></li><li><a href="wifi_printers.php">Wi-Fi Printing</a></li><li><a href="bluetooth_printers.php">Bluetooth Printing</a></li><li><a href="mac_printers.php">Mac Printing</a></li><li><a href="smb_printers.php">Windows Printing</a></li><li><a href="shared_printers.php">Shared Printing</a></li><li><a href="usb_printers.php">USB Printing</a></li><li><a href="gcloud_printers.php">Google Cloud Printing</a></li><li><a href="mobile_scanning.php">Mobile Scanning</a></li><li><a href="/docs/PrintHand_User_Guide.pdf" target="_blank">PrintHand User Guide (PDF)</a></li>
+<hr/>
+<li><a href="list_of_supported_printers.php" class="active">List of Supported Printers</a></li><li><a href="list_of_supported_portable_printers.php">List of Supported Portable Printers</a></li><li><a href="list_of_supported_scanners.php">List of Supported Scanners and Multi-functional Printers</a></li><li><a href="list_of_supported_languages.php">List of Supported Languages</a></li></ul>
+            </div>
+    </div>
+
+  </div>
+</div>
+
+<footer>
+<div class="container footer">
+  <div class="toolbar_shadow"></div>  
+<!-- ?php if ($p!='index') { ? -->
+
+  <div class="margin16">   
+    <div class="row-fluid">
+      <div class="row-fluid span4 footer">
+<p><b>PrintHand</b></p>  
+<a href="index.php">Home</a>
+<a href="download.php">Install</a>
+<a href="screen_shots.php">Screen Shots</a>
+<a href="faq.php">FAQ</a>
+<a href="help.php">Help</a>
+<a href="printhand_for_business.php">For Business</a>
+<a href="list_of_supported_printers.php">List of Supported Printers</a>
+<a href="list_of_supported_languages.php">List of Supported Languages</a>
+
+      </div>
+      <div class="row-fluid span3 footer">
+<p><b>Contact info &amp; Support</b></p>
+<a href="about_us.php">About Us</a>
+<a href="contact_us.php">Contact Us</a>
+<a href="feedback.php">Feedback</a>
+<a href="mailto:support@printhand.com">support@printhand.com</a>
+      </div>
+      <div class="row-fluid span3 footer">
+<p><b>Follow us</b></p>
+<a href="https://twitter.com/dynamixsoftware">Twitter</a>
+<a href="https://www.facebook.com/pages/Mobile-Dynamix/120680297971086">Facebook</a>
+<p><b>Choose language</b></p>
+<a href="/en/list_of_supported_printers.php" class="active">English</a>
+<a href="/ru/list_of_supported_printers.php">Русский</a>
+<a href="/cn/list_of_supported_printers.php">中文（简体)</a>
+      </div>
+      <div class="row-fluid span2 footer">
+<p><b>Legal</b></p>
+<a href="terms_of_use.php">Terms of Use</a>
+<a href="privacy_policy.php">Privacy Policy</a>
+<a href="trademark_certificate.php">Trademark Certificate</a>
+      </div>
+    </div>
+  </div>  
+
+<!-- ?php } ? -->
+
+  <div class="copyright">Site design / Logo &copy; 2012-2019 Dynamix Software. </div>
+</div>
+</footer>
+
+<script src="/js/jquery-1.9.1.min.js"></script>
+<script src="/js/bootstrap-transition.js"></script>
+<script src="/js/bootstrap-collapse.js"></script>
+<script src="/js/bootstrap-dropdown.js"></script>
+
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-5677218-7', 'printhand.com');
+  ga('send', 'pageview');
+
+</script>
+    </body>
+</html>
